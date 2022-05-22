c3d97172884121dc2c10395f6775b4e38ab940b9
Everton Ferreira <95659677+kirok3@users.noreply.github.com>
2022-05-22 15:27:28 -0300

    Funcionario
    Atualização

--- funcionario/util/config.php
+++ funcionario/util/config.php
@@ -1,12 +1,12 @@
 <?php
-    $ DB_SERVER = 'localhost' ;
-    $ DB_USER = 'root' ;
-    $ DB_PASSWORD = '' ;
-    $ DB_NAME = 'pi_upet' ;
+    $ DB_SERVER = 'localhost';
+    $ DB_USER = 'root';
+    $ DB_PASSWORD = '';
+    $ DB_NAME = 'pi_upet';
 
-    $ link = mysqli_connect ( $ DB_SERVER , $ DB_USER , $ DB_PASSWORD , $ DB_NAME );
+    $ link = mysqli_connect ($ DB_SERVER , $ DB_USER , $ DB_PASSWORD , $ DB_NAME);
 
-    if ( $ link === false ){
-        die ( "ERRO: não foi possivel realizar a conexão com o BD" );
+    if ($ link === false){
+        die ("ERRO: não foi possivel realizar a conexão com o BD");
     }
 ?>
